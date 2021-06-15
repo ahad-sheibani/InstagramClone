99314518ecb3a6f56d8822b39882a9531bd6d158
ahad-sheibani <ahad.sh1372@gmail.com>
2021-06-15 12:29:19 +0430

    last push

--- instagramclone/src/pages/profile.js
+++ instagramclone/src/pages/profile.js
@@ -1,37 +1,34 @@
-import { useHistory, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getUserByUsername } from "../services/firebase";
-import * as ROUTES from "../constants/routes";
+
+import { useParams, useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Profile() {
-    const { username } = useParams()
-    console.log("🚀 ~ file: profile.js ~ line 8 ~ Profile ~ username", username)
-    const [userExists, setUserExists] = useState(false)
-    const [user, setUser] = useState(null)
-    const history = useHistory()
+  const { username } = useParams()
+  const [userExists, setUserExists] = useState(false)
+  const [user, setUser] = useState(null)
+  
+  const history = useHistory()
+  console.log("🚀 ~ file: profile.js ~ line 9 ~ Profile ~ username", username)
 
-    useEffect(() => {
-        async function checkUserExists() {
-            //  const user = username;
-            const user = await getUserByUsername(username);
-            if (user.length > 0) {
-                setUser(user)
-                setUserExists(true);
-                console.log("🚀 ~ file: profile.js ~ line 19 ~ checkUserExists ~ setUserExists", setUserExists)
-            } else {
-                history.push(ROUTES.NOT_FOUND)
-            }
-        }
+  useEffect(() => {
+    async function checkUserExists() {
+      const [user] = await getUserByUsername(username);
+      console.log("🚀 ~ file: profile.js ~ line 18 ~ checkUserExists ~ user", user)
+      if (user?.userId) {
+        setUser(user);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
 
-        checkUserExists()
-        console.log('userr',user)
-    }, [username,history])
-
-    return userExists?(
-        <div className="bg-gray-background">
-            <div className="mx-auto max-w-screen-lg">
-                {/* {user.fullName} */}
-            </div>
-        </div>
-    ):null ;
+    checkUserExists();
+  }, [username, history]);
+  
+  return userExists?( 
+  <div className="bg-gray-background">
+    {username}
+  </div>
+  ): "ahad"
 }
--- instagramclone/src/seed.js
+++ instagramclone/src/seed.js
@@ -12,6 +12,15 @@ export function seedDatabase(firebase) {
         dateCreated: Date.now()
       },
       {
+        userId: 'NvPY9M9MzFTARQ6M816YAzDJxZ72',
+        username: 'karl',
+        fullName: 'Karl Hadwen',
+        emailAddress: 'karlhadwen@gmail.com',
+        following: ['2'],
+        followers: ['2', '3', '4'],
+        dateCreated: Date.now()
+      },
+      {
         userId: '2',
         username: 'raphael',
         fullName: 'Raffaello Sanzio da Urbino',
